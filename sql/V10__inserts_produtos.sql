94cf700bf3c3998768eeb64f72fc2deb5eb22703
marcelobrunocorrea <marcelobrunocorrea@yahoo.com.br>
2025-05-01 10:41:44 -0300

    versao 10

---
+++ sql/V10__inserts_produtos.sql
@@ -0,0 +1,14 @@
+-- V10__inserts_produtos.sql
+
+-- Inserções de dados iniciais na tabela produtos
+INSERT INTO produtos (nome, preco, estoque) VALUES 
+('Notebook Dell', 4200.00, 10),
+('Mouse Logitech', 120.00, 100),
+('Teclado Mecânico Redragon', 250.00, 50),
+('Monitor LG 24"', 980.00, 20),
+('HD Externo 1TB', 350.00, 30),
+('Webcam Full HD', 200.00, 40),
+('Fone de Ouvido JBL', 150.00, 60),
+('Impressora HP Deskjet', 470.00, 15),
+('Cadeira Gamer', 850.00, 5),
+('Pendrive 64GB', 60.00, 200);
