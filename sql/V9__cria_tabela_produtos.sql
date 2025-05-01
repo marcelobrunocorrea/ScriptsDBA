67596073be1949b86946925963f698c2000f3f8c
marcelobrunocorrea <marcelobrunocorrea@yahoo.com.br>
2025-04-30 21:08:36 -0300

    versao 1

--- sql/V9__cria_tabela_produtos.sql
+++
@@ -1,9 +0,0 @@
--- V2__cria_tabela_produtos.sql
-
-CREATE TABLE IF NOT EXISTS produtos (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco NUMERIC(10,2) NOT NULL,
-    estoque INT NOT NULL DEFAULT 0,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
